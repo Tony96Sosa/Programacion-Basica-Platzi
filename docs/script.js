eb6d00000ceee70b53d54b53c49db7e3b5e605fd
AnthonySosa <tony96sosa@hotmail.com>
2020-05-20 13:35:02 -0300

    usando eventos por primera vez

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,24 @@
-let g_tierra = 9.8;
-let g_marte = 3.7;
-let g_jupiter = 24.8;
-let peso = parseInt(prompt("¿Cual es tu peso?"));
-let planeta = prompt("Escriba el planeta para calcular tu peso:\n 1.Marte\n 2.Jupiter");
-let peso_final;
-if("marte"===(planeta.toLowerCase())){
-	peso_final = parseInt(peso*g_marte/g_tierra);
-}else{
-	peso_final = parseInt(peso*g_jupiter/g_tierra);
+let d = document.getElementById("canvas");
+let lienzo = d.getContext("2d");
+
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal){
+	lienzo.beginPath();
+	lienzo.strokeStyle = color;
+	lienzo.moveTo(xinicial,yinicial);
+	lienzo.lineTo(xfinal,yfinal);
+	lienzo.stroke();
+	lienzo.closePath();
 }
-document.write(`<h2 class="option">Tu peso en ${planeta} es ${peso_final}.</h2>`);
+
+
+let text = document.getElementById("lineas");
+let button = document.getElementById("form__boton");
+button.addEventListener("click", dibujoByClick);
+
+function dibujoByClick(){
+	let lineas = parseInt(text.value);
+	for(let i=0;i<lineas;i++){
+	dibujarLinea("red", 1, i*(300/lineas), (i+1)*(300/lineas), 299);
+	}
+}
